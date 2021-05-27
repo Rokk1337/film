0f33ad7722bea9180a9df54b06040801063cbb72
Rokki <9537425730artem@gmail.com>
2021-05-27 14:35:14 +0300

    урок 13

--- main.js
+++ main.js
@@ -1,19 +1,17 @@
-const numberOfFilms = +prompt("сколько фильмов вы уже посмотрели", "");
-console.log(numberOfFilms);
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
 
-const a = prompt("Один из последних фильмов?",""),
-      b = prompt("На сколько его оцените?"," "),
-      c = prompt("Один из последних фильмов?",""),
-      d = prompt("На сколько его оцените?"," ");
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-console.log(personalMovieDB);
+const num = 1;
+switch (num) {
+    case 49:
+        console.log('NO');
+        break;
+    case 100:
+    console.log('NO');
+        break;
+    case 50:
+        console.log('YES');
+        break;
+    default:
+        console.log('не сегодня');
+        break;
+}
